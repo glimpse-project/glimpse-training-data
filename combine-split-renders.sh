e041d0d23d1d0efe7a2897ca96df8e0ec6396bf9
Robert Bragg <robert@impossible.com>
2018-08-22 22:28:07 +0100

    combine-split-renders.sh: update in line with glimpse-render.py

--- combine-split-renders.sh
+++ combine-split-renders.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# render.sh creates multiple directories like renders/render-$NAME-$i
+# glimpse-render.py creates multiple directories like renders/$NAME-part-$i
 # but for pre-processing we want a single combined directory...
 
-if test $# != 2; then
-	echo "Usage: $0 NAME N"
+if test $# != 3; then
+	echo "Usage: $0 RENDERS_TOP_DIR NAME N"
 	exit 1
 fi
 
-NAME=$1
-N=$2
+RENDERS_TOP_DIR=$1
+NAME=$2
+N=$3
 
-cd renders
+cd $RENDERS_TOP_DIR
 
 mkdir -p $NAME/labels
 mkdir -p $NAME/depth
-cp render-$NAME-0/meta.json $NAME
+cp $NAME-part-0/meta.json $NAME
 
 for i in `seq 0 $(($N - 1))`
 do
-	echo "cp -a render-$NAME-$i/labels/* $NAME/labels ..."
-	cp -a render-$NAME-$i/labels/* $NAME/labels
-	echo "cp -a render-$NAME-$i/depth/* $NAME/depth ..."
-	cp -a render-$NAME-$i/depth/* $NAME/depth
+	echo "cp -a $NAME-part-$i/labels/* $NAME/labels ..."
+	cp -a $NAME-part-$i/labels/* $NAME/labels
+	echo "cp -a $NAME-part-$i/depth/* $NAME/depth ..."
+	cp -a $NAME-part-$i/depth/* $NAME/depth
 done
 
