d9456236988495dbb6fea874b498c75756597ce8
Robert Bragg <robert@impossible.com>
2018-12-10 23:36:23 +0000

    glimpse-build-training-jobs: support templates
    This adds support for a job template .json file that can provide default
    values for properties and support for index and out_file template
    strings.
    
    Within an index or out_file template (--index-template /
    --out-file-template) then '{job}' expands to the unique index of the
    job. Within an out_file template then '{index}' expands to the value of
    of the 'index' name property.
    
    This also makes some pedantic flake8 warning fixes; adds some comments
    for things I was initially a bit unsure of and re-works the recursion
    to simplify a bit (avoiding a special case for the first job).

--- glimpse-build-training-jobs.py
+++ glimpse-build-training-jobs.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
-import argparse, json, copy
+import argparse
+import json
+import copy
 
-parser = argparse.ArgumentParser(description=
-        'Generate job files for train_rdt',
-        epilog=
-        'Example usage: %(prog)s --param-set n_thresholds,100 --param-range uv_range,0.4,0.9,10')
 
 def linspace(min, max, n_values):
     step = (max - min) / (int(n_values) - 1)
@@ -15,6 +13,7 @@ def linspace(min, max, n_values):
         min += step
         i += 1
     yield max
+
 
 def parse_value(string):
     if string.lower() == 'true':
@@ -30,6 +29,7 @@ def parse_value(string):
         except ValueError:
             return string
 
+
 def parse_param(string):
     params = string.split(',')
     if len(params) != 2:
@@ -37,17 +37,20 @@ def parse_param(string):
         raise argparse.ArgumentTypeError(msg)
     return (params[0], [parse_value(params[1])])
 
+
 def parse_list(string):
     params = string.split(',')
     if len(params) < 3:
         msg = '%r does not have at least two values' % string
         raise argparse.ArgumentTypeError(msg)
-    return (params[0], map(lambda x:parse_value(x), params[1:]))
+    return (params[0], map(lambda x: parse_value(x), params[1:]))
+
 
 def parse_range(string):
     params = string.split(',')
     if len(params) != 4:
-        msg = '%r has an incorrect number of parameters, expected 4' % string
+        msg = ('%r has an incorrect number of parameters, expected 4 comma '
+               'separted values like: <name>,<min>,<max>,<N-steps>' % string)
         raise argparse.ArgumentTypeError(msg)
 
     min = parse_value(params[1])
@@ -67,45 +70,77 @@ def parse_range(string):
 
     return (params[0], list(linspace(min, max, n_values)))
 
-parser.add_argument('-o', '--output-prefix', type=str, default='out',
-                    help='Prefix for out_file property')
-parser.add_argument('-s', '--param-set', action='append', type=parse_param,
+
+parser = argparse.ArgumentParser(
+    description='Generate job files for train_rdt',
+    epilog='Example usage: %(prog)s --param-set n_thresholds,100 '
+           '--param-range uv_range,0.4,0.9,10')
+
+parser.add_argument('-t', '--template',
+                    help='A template job description with default values')
+parser.add_argument('-i', '--index-template', type=str,
+                    default='tree-{job}',
+                    help='Name of the training data index to load. '
+                         '"{job}" will be replace with the unique index of '
+                         'the job. '
+                         '(default=tree-{job})')
+parser.add_argument('-o', '--out-file-template', type=str,
+                    default='{index}.json',
+                    help='Name for output trees. "{index}" will be '
+                         'replaced with the name of the data index associated '
+                         'with the job. "{job}" will be replace with the '
+                         'unique index of the job. '
+                         '(default={index}.json)')
+parser.add_argument('-s', '--param-set', action='append',
+                    default=[], type=parse_param,
                     help='Set a named parameter')
-parser.add_argument('-l', '--param-list', action='append', type=parse_list,
+parser.add_argument('-l', '--param-list', action='append',
+                    default=[], type=parse_list,
                     help='Vary a named parameter with these specific values')
-parser.add_argument('-r', '--param-range', action='append', type=parse_range,
-                    help='Vary a number parameter over a range. e.g. -r example,0.1,0.9,10')
+parser.add_argument('-r', '--param-range', action='append',
+                    default=[], type=parse_range,
+                    help='Vary a given parameter over a range, described with '
+                         'four comma separated components: '
+                         '<name>,<min>,<max><N-steps> '
+                         '(e.g. --param-rage example_prop,0.1,0.9,10)')
 
 args = parser.parse_args()
-args_list = [args.param_set, args.param_list, args.param_range]
-props = [item for sublist in [x for x in args_list if x is not None] for item in sublist]
 
-output_id = 0
-def add_jobs_recursive(jobs=[], job=None, n_prop=0):
-    global output_id, props
+# After the parsing that's done by the custom parsers given for these arguments
+# we will end up with a props array like:
+#
+#   [(prop0, [val0, val1, val2]), (prop1, [val0, val1])]
+#
+props = args.param_set + args.param_list + args.param_range
+out_file_template = args.out_file_template
+index_template = args.index_template
+job_id = 0
 
-    if n_prop >= len(props):
-        return jobs
 
-    if job is None:
-        job = {}
-        job['out_file'] = '%s-%05d.json' % (args.output_prefix, output_id)
-        output_id += 1;
-        jobs.append(job)
+def build_jobs_recursive(job, prop_index=0):
+    global job_id
+    jobs = []
 
-    prop = props[n_prop]
-    job[prop[0]] = prop[1][0]
-    add_jobs_recursive(jobs, job, n_prop + 1)
+    if prop_index >= len(props):
+        job['index'] = index_template.format(job=job_id)
+        job['out_file'] = out_file_template.format(index=job['index'],
+                                                   job=job_id)
+        job_id += 1
+        return [job]
 
-    for i in range(1, len(prop[1])):
-        job_copy = copy.deepcopy(job)
-        job_copy['out_file'] = '%s-%05d.json' % (args.output_prefix, output_id)
-        output_id += 1;
-        jobs.append(job_copy)
-        job_copy[prop[0]] = prop[1][i]
-        add_jobs_recursive(jobs, job_copy, n_prop + 1)
+    prop = props[prop_index]
+    for val in prop[1]:
+        job[prop[0]] = val
+        jobs += build_jobs_recursive(copy.deepcopy(job), prop_index + 1)
 
     return jobs
 
-jobs = add_jobs_recursive()
+
+if args.template:
+    with open(args.template, 'r') as fp:
+        job_template = json.load(fp)
+else:
+    job_template = {}
+
+jobs = build_jobs_recursive(copy.deepcopy(job_template))
 print(json.dumps(jobs, indent=2))
